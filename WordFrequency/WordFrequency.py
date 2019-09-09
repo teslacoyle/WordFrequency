5869055904429f70ead4b0f4e58fed331a25be58
Eli Coyle <elijcoyle@outlook.com>
2019-09-09 11:55:21 -0400

    added functionality to assess frequency of text excluding stopwords

--- WordFrequency/WordFrequency.py
+++ WordFrequency/WordFrequency.py
@@ -1,20 +1,27 @@
 from collections import defaultdict
 import re, sys
 
+stops = open('stopwords.txt')
+stopwords = set(line.rstrip('\n') for line in stops)
+stops.close()
+
 def WordFrequency(filename):
     freq_dict = defaultdict(int)
+    freq_dict_nostops = defaultdict(int)
     try:
         file = open(filename)
     except:
-        print("Excepotion thrown while trying to open filename: {}".format(filename))
+        print("Exception thrown while trying to open filename: {}".format(filename))
     input_text = file.read()
     file.close()
     input_text_clean = re.sub(r'[^\' a-zA-Z0-9]', ' ', input_text)
     words_list = input_text_clean.split()
 
     for w in words_list:
-        # if w not in stop_words (indent lower line):
         freq_dict[w.lower()] += 1
+        if w.lower() not in stopwords:
+            freq_dict_nostops[w.lower()] += 1
+            
 
     sorted_words = sorted(freq_dict, key=freq_dict.get, reverse=True)
     word_count = len(words_list)
@@ -23,13 +30,19 @@ def WordFrequency(filename):
     print("({} words total in {})".format(word_count, filename))
     for word, freq in zip(sorted_words[0:5], percentages):
         print("{0}\t {1}\t {2:.3f}%".format(word, freq_dict[word], freq*100))
+
+    print('\n\n')
+
+    sorted_words_nostops = sorted(freq_dict_nostops, key=freq_dict_nostops.get, reverse=True)
+    percentages_nostops = [freq_dict_nostops[w]/word_count for w in sorted_words_nostops[0:5]]
+    print('MOST COMMON WORDS in {} (stopwords removed):\n'.format(filename))
+    print("({} words total in {})".format(word_count, filename))
+    for word, freq in zip(sorted_words_nostops[0:5], percentages_nostops):
+        print("{0}\t {1}\t {2:.3f}%".format(word, freq_dict_nostops[word], freq*100))
     return
 
-# (read and split the list of stopwords that you download from the internet)
-# stop_words = open("./input_text.txt").read().split("\n")
-
-if sys.argv[0] is not None:
-    WordFrequency(str(sys.argv[0]))
+if sys.argv[1] is not None:
+    WordFrequency(str(sys.argv[1]))
 else:
     print("Please specify a .txt filename as a command line argument.")
     quit()
